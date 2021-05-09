801fadd2a7fc7e8c2e585744abcf949fd4638d36
Rendani <rendaniluvhengo89@gmail.com>
2021-05-09 23:38:12 +0200

    Added redux-form

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import {connect} from "react-redux"
 import {increaseCounter, decreaseCounter} from "./redux/Counter/counter.actions"
-import { DECREMENT } from './redux/Counter/counter.types';
+//import { DECREMENT } from './redux/Counter/counter.types';
 
 
 
 function App(props: any) {
   return (
     <div className="App">
+      <div className="input"><input type="text" /></div>
    <div> Count: {props.count}</div>
    <button onClick={props.increment }>Increment</button>
    <button onClick={props.decrement}>Decrement</button>
@@ -16,7 +17,7 @@ function App(props: any) {
   );
 }
 
-// how ot get data from the redux into our props
+// how to get data from the redux into our props
 function mapStateToProps(state: any){
   return {
     count: state.counter.count
@@ -27,9 +28,10 @@ function mapStateToProps(state: any){
 function mapDispatchToProps(dispatch:any){
   return {
     increment: () => (dispatch(increaseCounter())),
-    decrement: () => (dispatch({
-      type: DECREMENT,
-    }))
+  /*   decrement: () => (dispatch({
+  type: DECREMENT  })) */
+decrement: () => (dispatch(decreaseCounter())),
+  
   }
 }
 
--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form'
 
 import counterReducer from './Counter/counter.reducer';
 
 const rootReducer = combineReducers({
     counter: counterReducer,
+    form: formReducer,
 });
 
 
