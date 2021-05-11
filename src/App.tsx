0893bda161d4ce57703573b11813679eead70520
Rendani <rendaniluvhengo89@gmail.com>
2021-05-11 13:51:16 +0200

    refractoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,82 @@
-import React from 'react';
-import './App.css';
-import {connect} from "react-redux"
-import {increaseCounter, decreaseCounter} from "./redux/Counter/counter.actions"
-//import { DECREMENT } from './redux/Counter/counter.types';
-
-
+import React, { useState } from "react";
+import "./App.css";
+import { connect } from "react-redux";
 
 function App(props: any) {
+  const [inputValue, setInputValue] = useState(0);
+
   return (
     <div className="App">
-      <div className="input"><input type="text" /></div>
-   <div> Count: {props.count}</div>
-   <button onClick={props.increment }>Increment</button>
-   <button onClick={props.decrement}>Decrement</button>
+      <div className="input">
+        <input
+          type="number"
+          value={inputValue}
+          onChange={(e) => {
+            setInputValue(parseFloat(e.target.value));
+          }}
+        />
+        <button
+          onClick={() => {
+            props.addValue(inputValue);
+            setInputValue(0);
+          }}
+        >
+          Add
+        </button>
+        <button
+          onClick={() => {
+            props.minusValue(inputValue);
+            setInputValue(0);
+          }}
+        >
+          Minus
+        </button>
+      </div>
+      <div> Count: {props.count}</div>
+      <button onClick={props.increment}>Increment</button>
+      <button onClick={props.decrement}>Decrement</button>
     </div>
   );
 }
 
 // how to get data from the redux into our props
-function mapStateToProps(state: any){
+function mapStateToProps(state: any) {
   return {
-    count: state.counter.count
-  }
+    count: state.count,
+  };
 }
 
 // how ot get data from the redux into our props
-function mapDispatchToProps(dispatch:any){
+function mapDispatchToProps(dispatch: any) {
   return {
-    increment: () => (dispatch(increaseCounter())),
-  /*   decrement: () => (dispatch({
-  type: DECREMENT  })) */
-decrement: () => (dispatch(decreaseCounter())),
-  
-  }
+    increment: () =>
+      dispatch({
+        type: "INCREMENT",
+      }),
+    decrement: () =>
+      dispatch({
+        type: "DECREMENT",
+      }),
+    addValue: (value: number) =>
+      dispatch({
+        type: "ADD_VALUE",
+        payload: value,
+      }),
+    minusValue: (value: number) =>
+      dispatch({
+        type: "MINUS_VALUE",
+        payload: value,
+      }),
+  };
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
+
+// TODOS
+// Add a new button to divide the state by a value
+// Add a new button to multipy the state by a value
+
+// Adding a new state in the store
+// Add a new redux state that is an array
+// In the initial moment is an empty array
+// Add a button that can add a bnumber into the array
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,36 @@
 import { createStore } from 'redux';
-import rootReducer from './rootReducer';
 
-const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const initialState = {
+    count: 0,
+};
+
+function reducer (state = initialState, action)  {
+    switch (action.type) {
+        case "INCREMENT":
+           return {
+             ...state, // return old state
+             count: state.count + 1, // cahnge the count property to count +1
+           };
+        case "DECREMENT":
+           return {
+              ...state, 
+              count: state.count - 1,
+           };
+           case "ADD_VALUE":
+            return {
+               ...state, 
+               count: state.count + action.payload,
+            };
+            case "MINUS_VALUE":
+              return {
+                 ...state, 
+                 count: state.count - action.payload,
+              };
+          
+         default: return state;
+    }
+};
+
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export default store;
